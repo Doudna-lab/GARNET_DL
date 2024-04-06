607eddb3151fb0634bcba18b30dac520e334c40b
Marena Trinidad <marenatrinidad@gmail.com>
2024-04-05 23:26:13 -0700

    Delete Preprocess_Data directory

--- Preprocess_Data/Contact_Maps/contact_map_generator/build/lib/contact_map_generator/common/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base  import *
-from .bio  import *
-from .matvec import *
-from .sys import *
-
-#__all__ = ['base', 'bio', 'matvec', 'sys']
--- Preprocess_Data/Contact_Maps/contact_map_generator/contact_map_generator/pdblib2/toppar/test.py
+++
@@ -1,5 +0,0 @@
-lines=open('parm.dat').readlines()
-
-for i in range(10000):
-#   filter(lambda x: x[:1]!='#', lines)
-    [line for line in lines if line[:1]!='#']
